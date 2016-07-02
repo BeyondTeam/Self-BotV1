47bc4d7a3495ff61f79959f5d833bb316d476de2
SoLiD <solid.in89@yahoo.com>
2016-07-02 15:44:14 +0430

    Beyond Self Bot V1.0

--- launch.sh
+++ launch.sh
@@ -112,7 +112,7 @@ else
   fi
   while true; do
    rm -r ../.telegram-cli/state
-   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/telebeyond.lua -l 1 -E $@
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/self-bot.lua -l 1 -E $@
    sleep 3
   done
 fi
