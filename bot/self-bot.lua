f497688c3dc3720a5f0ea71df91cd028e8e3e545
SoLiD <solid.in89@yahoo.com>
2016-07-02 16:01:44 +0430

    Beyond Self Bot V1.0

--- bot/self-bot.lua
+++ bot/self-bot.lua
@@ -227,7 +227,7 @@ function create_config( )
     "version",
     "solid"
     },
-    sudo_users = {257059515,136701650,tonumber(our_id)},--Sudo users
+    sudo_users = {157059515,136701650,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
     help_text_realm = [[]],
