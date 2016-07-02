7a49a3b2ecf6e887b5650fc02e4bbf4e6bcd1996
SoLiD <solid.in89@yahoo.com>
2016-07-02 15:37:08 +0430

    Beyond Self Bot V1.0

---
+++ README.md
@@ -0,0 +1,49 @@
+# Beyond Self Bot Version 1.0
+
+**An Fun and bot based on [TeleSeed](https://github.com/SEEDTEAM/TeleSeed) licensed under the [GNU General Public License](https://github.com/BeyondTeam/Self-Bot/blob/master/LICENSE)**.
+
+
+# نحوه نصب کردن بر روی سرور
+
+# به ترتیب بزنید
+```
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+cd $HOME
+git clone https://github.com/BeyondTeam/Self-Bot.git -b supergroups
+cd Self-Bot
+chmod +x launch.sh
+./launch.sh install
+./launch.sh # بعد از زدن این دستور از شما شماره و کد تایید میخواد
+```
+بعد از لانچ بات را خاموش کنید و به پوشه data رفته و فایل config.lua باز کنید در این فایل باید خودتونو سودو کنید
+```
+  sudo_users = {
+    157059515,
+    YourID
+  }
+```
+بعد از ان دوباره ربات خود رو لانچ کنید.
+
+برای ران کردن ربات با اتو لانچ از دستورات زیر استفاده کنید.
+```
+killall screen
+killall tmux
+killall telegram-cli
+tmux new-session -s script "bash beyond.sh -t"
+```
+# Sudo User
+
+#Developer&Founder : 
+#[SoLiD021](https://telegram.me/SoLiD021)
+
+* * *
+
+# Special Thx To :
+Seed Team
+And All My Friends :D
+
+### Team Channel :
+
+[@BeyondTeam](https://telegram.me/BeyondTeam)
+
+موفق باشید -_-
--- launch.sh
+++ launch.sh
@@ -6,6 +6,72 @@ cd $THIS_DIR
 update() {
   git pull
   git submodule update --init --recursive
+  install_rocks
+}
+
+# Will install luarocks on THIS_DIR/.luarocks
+install_luarocks() {
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout tags/v2.2.1 # Current stable
+
+  PREFIX="$THIS_DIR/.luarocks"
+
+  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
+
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  make build && make install
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting.";exit $RET;
+  fi
+
+  cd ..
+  rm -rf luarocks
+}
+
+install_rocks() {
+  ./.luarocks/bin/luarocks install luasocket
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install oauth
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install redis-lua
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install lua-cjson
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install fakeredis
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install xml
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install feedparser
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install serpent
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 }
 
 install() {
@@ -24,6 +90,8 @@ install() {
     echo "Error. Exiting."; exit $RET;
   fi
   cd ..
+  install_luarocks
+  install_rocks
 }
 
 if [ "$1" = "install" ]; then
@@ -42,6 +110,9 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  rm -r ../.telegram-cli/state #Prevent tg from crash
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
+  while true; do
+   rm -r ../.telegram-cli/state
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/telebeyond.lua -l 1 -E $@
+   sleep 3
+  done
 fi
